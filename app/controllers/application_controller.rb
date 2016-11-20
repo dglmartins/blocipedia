140e752fab297a014a2b2cc6d8a11adbbceacee3
danilomartins <dglmartins@outlook.com>
2016-11-20 23:03:49 +0800

    user story 1 completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # add this before action to the controller of the view the user needs to be authenticated to see.
+  #before_action :authenticate_user!
 end
