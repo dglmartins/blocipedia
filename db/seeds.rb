799568297ba4a455bf6a28abd24fd5d38557b3d7
danilomartins <dglmartins@outlook.com>
2016-11-29 11:02:57 +0800

    checkpoint 6 seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,79 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  password = Faker::Internet.password
+  User.create!(
+  email: Faker::Internet.email,
+  password: password,
+  password_confirmation: password
+  )
+end
+
+standard_users = User.where(role: 'standard')
+
+#premium users
+10.times do
+  password = Faker::Internet.password
+  User.create!(
+  email: Faker::Internet.email,
+  password: password,
+  password_confirmation: password,
+  role: 'premium'
+  )
+end
+
+premium_users = User.where(role: 'premium')
+
+#premium users Wikis
+10.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    private: Faker::Boolean.boolean,
+    user: premium_users.sample
+  )
+end
+
+#standard users Wikis
+50.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    private: false,
+    user: standard_users.sample
+  )
+end
+
+admin = User.create!(
+  email: 'admin@blocipedia.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld',
+  role: 'admin'
+)
+
+standardMember = User.create!(
+  email: 'standard_member@blocipedia.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld',
+  role: 'standard'
+)
+
+standardMember = User.create!(
+  email: 'premium_member@blocipedia.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld',
+  role: 'standard'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{User.where(role: 'standard').count} standard users created"
+puts "#{User.where(role: 'premium').count} premium users created"
+puts "#{User.where(role: 'admin').count} admin users created"
+puts "#{Wiki.count} wikis created"
+# puts "#{Wiki.where(user: == 'premium').count} wikis created by premium users"
+# puts "#{Wiki.where(user: == 'premium').count} wikis created by premium users"
+puts "#{Wiki.where(private: false).count} public wikis created"
+puts "#{Wiki.where(private: true).count} private wikis created"
